4e9dfb5209a75767b8582069b20d544dcd301480
DESKTOP-4QB4TVC\Diego Belini <drxbelini@gmail.com>
2019-06-21 11:17:03 -0300

    Ray cast method; Damage enemy scrit done; Muzzle FX add; Hit FX add;

---
+++ Assets/Scripts/EnemyHelth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHelth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    public void hitPointsDecresser(float weaponDamage)
+    {   
+        hitPoints -= weaponDamage;
+        
+        if (hitPoints <= 0 )
+        {
+            Destroy(gameObject);
+        }
+        print(hitPoints);
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,26 +7,57 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
+    [SerializeField] float weaponDamage = 10f;    
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitFx;
 
     void Update()
     {
-       if (Input.GetMouseButton(0))
+       if (Input.GetMouseButtonDown(0))
        {
            Shoot();
        }
     }
 
     private void Shoot()
-    {           
+    {
+        PlayMuzzleFlash();
+        ProcessRayCast();
+
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+
+    private void ProcessRayCast()
+    {
         RaycastHit hit;
-       
+
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             print(hit.transform.name);
+            
+            processHitFX(hit);
+
+            EnemyHelth target = hit.transform.GetComponent<EnemyHelth>();
+            if (target)
+            {
+                target.hitPointsDecresser(weaponDamage);
+            }
+
         }
         else
         {
             return;
         }
+    }
+
+    private void processHitFX(RaycastHit hit)
+    {
+       GameObject impact = Instantiate (hitFx, hit.point, Quaternion.LookRotation(hit.normal));
+       Destroy(impact, 2f);
     }
 }
